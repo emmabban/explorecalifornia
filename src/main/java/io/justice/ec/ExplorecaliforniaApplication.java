3a1c2039aa390043b16fd485491131e47bacc98e
Emmanuel Abban <emmanuel.abban251@gmail.com>
2022-10-12 03:43:00 -0400

    project initialized

---
+++ src/main/java/io/justice/ec/ExplorecaliforniaApplication.java
@@ -0,0 +1,13 @@
+package io.justice.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliforniaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/justice/ec/ExplorecaliforniaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.justice.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliforniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
